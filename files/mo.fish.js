7dc16780347923d551791f2a47c50806303f0df3
zhihaofans <zhihaofans@users.noreply.github.com>
2020-01-24 12:39:46 +0800

    Update mo.fish.js

--- files/mo.fish.js
+++ files/mo.fish.js
@@ -1,26 +1,37 @@
+var catList = [];
 var siteList = [];
 var siteItemList = [];
+var siteTitle = "";
 const isCache = false;
-const listViewId = "main-list";
+const listViewId = "listView_main";
 const cacheId = {
   siteList: "SITE_LIST",
   siteInfo: "SITE_INFO_",
   lastCacheTime: "LAST_CACHE_TIME"
 };
 function init() {
+  $ui.loading("加载中...");
+  getType();
+}
+// 旧版获取站点列表
+function getType() {
   const siteListCache = getCache(cacheId.siteList);
-  if (siteListCache == undefined || !isCache) {
+  if (!isCache || siteListCache == undefined) {
     const urlSiteList = "https://www.tophub.fun:8080/GetType";
     $http.get({
       url: urlSiteList,
-      handler: function(resp) {
+      handler: function (resp) {
         const mData = resp.data;
-        $console.log("siteList使用在线数据");
+        $console.log(
+          "siteList: " + mData.Code.toString() + " | " + mData.Message
+        );
         setCache(cacheId.siteList, mData);
+        $ui.loading(false);
         checkSiteList(mData);
       }
     });
   } else {
+    $ui.loading(false);
     $console.log("siteList使用缓存数据");
     checkSiteList(siteListCache);
   }
@@ -39,7 +50,7 @@ function checkSiteList(siteListData) {
         itemTitleList.push(thisItem.title);
       }
       $console.log(itemTitleList);
-      showSiteList(itemTitleList);
+      pushSiteList(itemTitleList);
     }
   } else {
     $ui.error(siteListData.Message);
@@ -47,8 +58,12 @@ function checkSiteList(siteListData) {
   }
 }
 // 渲染站点列表
-function showSiteList(itemList) {
+function pushSiteList(itemList) {
   $ui.render({
+    props: {
+      id: "listView_index",
+      title: "加载完毕"
+    },
     views: [
       {
         type: "list",
@@ -58,43 +73,104 @@ function showSiteList(itemList) {
         },
         layout: $layout.fill,
         events: {
-          didSelect: function(sender, indexPath, data) {
+          didSelect: function (sender, indexPath, data) {
             const mIndex = indexPath.row;
             const selectSite = siteList[mIndex];
-            $ui.title = selectSite.title;
-            getSiteInfo(selectSite.id);
+            getSiteInfo(selectSite.id, selectSite.title);
           }
         }
       }
     ]
   });
+  //console.log($ui.get("listView_index"));
 }
-
+// 按分类获取站点列表
+function getAllType() {
+  const urlAllType = "https://www.tophub.fun:8888/GetAllType";
+  $http.get({
+    url: urlAllType,
+    handler: function (resp) {
+      const mData = resp.data;
+      $console.log("newSiteList使用在线数据");
+      $ui.loading(false);
+      showNewSiteList(mData);
+    }
+  });
+}
+// 处理站点列表数据
+function showNewSiteList(siteCatListData) {
+  if (siteCatListData.Code == 0) {
+    catList = siteCatListData.Data;
+    if (catList.length == 0) {
+      $ui.error("空白列表");
+    } else {
+      var itemTitleList = [];
+      for (x in catList) {
+        $console.log(x);
+        itemTitleList.push();
+      }
+      $console.log(siteCatListData);
+      pushSiteList(itemTitleList);
+    }
+  } else {
+    $ui.error(siteCatListData.Message);
+  }
+}
+// 渲染站点分类列表
+function pushSiteCatList(itemList) {
+  $ui.render({
+    props: {
+      id: "listView_cat",
+      title: "站点分类"
+    },
+    views: [
+      {
+        type: "list",
+        props: {
+          data: itemList,
+          id: listViewId
+        },
+        layout: $layout.fill,
+        events: {
+          didSelect: function (sender, indexPath, data) {
+            const mIndex = indexPath.row;
+            const selectSite = catList[mIndex];
+            getSiteInfo(selectSite.id, selectSite.title);
+          }
+        }
+      }
+    ]
+  });
+  //$console.log($ui.get("listView_cat"));
+}
 // 获取站点内容
-function getSiteInfo(siteId) {
+function getSiteInfo(siteId, title) {
+  $ui.loading("加载中...");
   siteItemList = [];
   const cacheSiteId = cacheId.siteInfo + siteId;
   const siteInfoCache = getCache(cacheSiteId);
   if (siteInfoCache == undefined || !isCache) {
     const urlSiteInfo =
-      "https://www.tophub.fun:8080/GetAllInfoGzip?id=" + siteId.toString();
+      "https://www.tophub.fun:8888/GetAllInfoGzip?id=" + siteId.toString();
     $http.get({
       url: urlSiteInfo,
-      handler: function(resp) {
+      handler: function (resp) {
         const itemListData = resp.data;
         setCache(cacheSiteId, itemListData);
         $console.log("siteInfo使用在线数据");
-        checkSiteInfo(itemListData, siteId);
+        $ui.loading(false);
+        checkSiteInfo(itemListData, siteId, title);
       }
     });
   } else {
     $console.log(cacheSiteId + ":" + JSON.stringify(siteInfoCache));
     $console.log("siteInfo使用缓存数据");
-    checkSiteInfo(siteInfoCache, siteId);
+    $ui.loading(false);
+    checkSiteInfo(siteInfoCache, siteId, title);
   }
 }
 // 处理站点内容数据
-function checkSiteInfo(siteInfoData, siteId) {
+function checkSiteInfo(siteInfoData, siteId, title) {
   $console.log(siteInfoData);
   const cacheSiteId = cacheId.siteInfo + siteId;
   if (siteInfoData.Code == 0) {
@@ -109,7 +185,7 @@ function checkSiteInfo(siteInfoData, siteId) {
         itemTitleList.push(thisItem.Title);
       }
       //$ui.title=sit
-      showSiteItemList(itemTitleList);
+      showSiteItemList(itemTitleList, title);
     }
   } else {
     $ui.error(siteInfoData.Message);
@@ -117,8 +193,11 @@ function checkSiteInfo(siteInfoData, siteId) {
   }
 }
 // 渲染站点内容列表
-function showSiteItemList(siteItemTitleList) {
+function showSiteItemList(siteItemTitleList, title) {
   const mPage = {
+    props: {
+      title: title
+    },
     views: [
       {
         type: "list",
@@ -128,7 +207,7 @@ function showSiteItemList(siteItemTitleList) {
         },
         layout: $layout.fill,
         events: {
-          didSelect: function(sender, indexPath, data) {
+          didSelect: function (sender, indexPath, data) {
             const mIndex = indexPath.row;
             const selectItem = siteItemList[mIndex];
             const itemUrl = selectItem.Url;
