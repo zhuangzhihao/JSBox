f1a005839c0bf3aea136cb76207679a3c713977c
zhihaofans <zhihaofans@hotmail.com>
2020-01-31 17:58:26 +0800

    在地区排序的列表里附带确诊人数

--- files/丁香园全国新型肺炎疫情实时动态.js
+++ files/丁香园全国新型肺炎疫情实时动态.js
@@ -17,9 +17,11 @@ var _imgList = {
 function json2string(_sourceJson) {
     return JSON.stringify(_sourceJson);
 }
+
 function getRealUrl(_sourceUrl) {
     return _sourceUrl.split(" ​ ")[0];
 }
+
 function initMainMenu() {
     isLoading = true;
     $ui.loading("正在加载在线数据");
@@ -29,56 +31,56 @@ function initMainMenu() {
             id: "listView_index",
             title: "全国新型肺炎疫情实时动态"
         },
-        views: [
-            {
-                type: "list",
-                props: {
-                    data: _menuList
-                },
-                layout: $layout.fill,
-                events: {
-                    didSelect: function(_sender, indexPath, _data) {
-                        if (isLoading) {
-                            $ui.error("请等待加载数据");
-                        } else {
-                            const _idx = indexPath.row;
-                            switch (_idx) {
-                                case 0:
-                                    showHeaderData();
-                                    break;
-                                case 1:
-                                    showMainData();
-                                    break;
-                                case 2:
-                                    showTimeLineData();
-                                    break;
-                                case 3:
-                                    showRumorData();
-                                    break;
-                                case 4:
-                                    showAreaStatData();
-                                    break;
-                                default:
-                                    $ui.error("错误选项");
-                            }
+        views: [{
+            type: "list",
+            props: {
+                data: _menuList
+            },
+            layout: $layout.fill,
+            events: {
+                didSelect: function (_sender, indexPath, _data) {
+                    if (isLoading) {
+                        $ui.error("请等待加载数据");
+                    } else {
+                        const _idx = indexPath.row;
+                        switch (_idx) {
+                            case 0:
+                                showHeaderData();
+                                break;
+                            case 1:
+                                showMainData();
+                                break;
+                            case 2:
+                                showTimeLineData();
+                                break;
+                            case 3:
+                                showRumorData();
+                                break;
+                            case 4:
+                                showAreaStatData();
+                                break;
+                            default:
+                                $ui.error("错误选项");
                         }
                     }
                 }
             }
-        ]
+        }]
     });
 }
+
 function getData() {
     const urlAllType = "https://3g.dxy.cn/newh5/view/pneumonia";
     $http.get({
         url: urlAllType,
-        handler: function(_resp) {
+        handler: function (_resp) {
             const mData = _resp.data;
             $console.log("获取数据成功");
             processData(mData);
         }
     });
 }
+
 function processData(_sourceData) {
     const doc = $xml.parse({
         string: _sourceData,
@@ -111,9 +113,11 @@ function getHeaderData(_element) {
         }
     });
 }
+
 function processHeaderData(_jsonData) {
     return JSON.parse(_jsonData);
 }
+
 function showHeaderData() {
     var messageText =
         "确诊 " +
@@ -139,18 +143,17 @@ function showHeaderData() {
     $ui.alert({
         title: _headerDataJson.virus,
         message: messageText,
-        actions: [
-            {
+        actions: [{
                 title: "分享内容",
                 disabled: false, // Optional
-                handler: function() {
+                handler: function () {
                     $share.sheet([messageText]);
                 }
             },
             {
                 title: "疫情地图",
                 disabled: false, // Optional
-                handler: function() {
+                handler: function () {
                     $ui.preview({
                         title: "疫情地图",
                         url: _imgList.imgUrl
@@ -160,7 +163,7 @@ function showHeaderData() {
             {
                 title: "全国疫情形势图",
                 disabled: false, // Optional
-                handler: function() {
+                handler: function () {
                     $ui.preview({
                         title: "全国疫情形势图",
                         url: _imgList.dailyPic
@@ -170,7 +173,7 @@ function showHeaderData() {
             {
                 title: "关闭",
                 disabled: false, // Optional
-                handler: function() {}
+                handler: function () {}
             }
         ]
     });
@@ -191,6 +194,7 @@ function getMainData(_element) {
         }
     });
 }
+
 function processMainData(_html) {
     var proList = [];
     const _json = JSON.parse(_html);
@@ -201,28 +205,28 @@ function processMainData(_html) {
     }
     return proList;
 }
+
 function showMainData() {
     $ui.push({
         props: {
             title: "各省数据"
         },
-        views: [
-            {
-                type: "list",
-                props: {
-                    data: _mainTitleDataJson
-                },
-                layout: $layout.fill,
-                events: {
-                    didSelect: function(_sender, indexPath, _data) {
-                        const _idx = indexPath.row;
-                        showProInfo(_idx);
-                    }
+        views: [{
+            type: "list",
+            props: {
+                data: _mainTitleDataJson
+            },
+            layout: $layout.fill,
+            events: {
+                didSelect: function (_sender, indexPath, _data) {
+                    const _idx = indexPath.row;
+                    showProInfo(_idx);
                 }
             }
-        ]
+        }]
     });
 }
+
 function showProInfo(_idx) {
     const _jsonData = _mainDataJson[_idx];
     const updateTime = Math.round((new Date() - _jsonData.modifyTime) / 1000);
@@ -284,6 +288,7 @@ function getTimeLine(_element) {
         }
     });
 }
+
 function processTimeLineData(_html) {
     var timeLineList = [];
     const _json = JSON.parse(_html);
@@ -294,28 +299,28 @@ function processTimeLineData(_html) {
     }
     return timeLineList;
 }
+
 function showTimeLineData() {
     $ui.push({
         props: {
             title: "时间线"
         },
-        views: [
-            {
-                type: "list",
-                props: {
-                    data: _timeLineTitleData
-                },
-                layout: $layout.fill,
-                events: {
-                    didSelect: function(_sender, indexPath, _data) {
-                        const _idx = indexPath.row;
-                        showTimeLineDetailedData(_idx);
-                    }
+        views: [{
+            type: "list",
+            props: {
+                data: _timeLineTitleData
+            },
+            layout: $layout.fill,
+            events: {
+                didSelect: function (_sender, indexPath, _data) {
+                    const _idx = indexPath.row;
+                    showTimeLineDetailedData(_idx);
                 }
             }
-        ]
+        }]
     });
 }
+
 function showTimeLineDetailedData(_idx) {
     const thisItem = _timeLineData[_idx];
     const _title = thisItem.title;
@@ -325,11 +330,10 @@ function showTimeLineDetailedData(_idx) {
     $ui.alert({
         title: _updateDate,
         message: _message,
-        actions: [
-            {
+        actions: [{
                 title: "打开链接",
                 disabled: false, // Optional
-                handler: function() {
+                handler: function () {
                     $ui.preview({
                         title: _title,
                         url: _url
@@ -347,10 +351,10 @@ function showTimeLineDetailedData(_idx) {
             },
             {
                 title: "分享链接",
-                handler: function() {
+                handler: function () {
                     $share.sheet({
                         items: _url,
-                        handler: function(success) {
+                        handler: function (success) {
                             if (success) {
                                 $ui.toast("分享成功");
                             } else {
@@ -362,7 +366,7 @@ function showTimeLineDetailedData(_idx) {
             },
             {
                 title: "关闭",
-                handler: function() {}
+                handler: function () {}
             }
         ]
     });
@@ -392,28 +396,28 @@ function processRumorData(_html) {
     }
     return rumorList;
 }
+
 function showRumorData() {
     $ui.push({
         props: {
             title: "谣言"
         },
-        views: [
-            {
-                type: "list",
-                props: {
-                    data: _rumorTitleData
-                },
-                layout: $layout.fill,
-                events: {
-                    didSelect: function(_sender, indexPath, _data) {
-                        const _idx = indexPath.row;
-                        showRumorDetailedData(_idx);
-                    }
+        views: [{
+            type: "list",
+            props: {
+                data: _rumorTitleData
+            },
+            layout: $layout.fill,
+            events: {
+                didSelect: function (_sender, indexPath, _data) {
+                    const _idx = indexPath.row;
+                    showRumorDetailedData(_idx);
                 }
             }
-        ]
+        }]
     });
 }
+
 function showRumorDetailedData(_idx) {
     const thisItem = _rumorData[_idx];
     const _title = thisItem.title;
@@ -425,11 +429,10 @@ function showRumorDetailedData(_idx) {
     $ui.alert({
         title: _title,
         message: thisItem.body,
-        actions: [
-            {
+        actions: [{
                 title: "打开链接",
                 disabled: isNotUrl, // Optional
-                handler: function() {
+                handler: function () {
                     $ui.preview({
                         title: _title,
                         url: _url
@@ -442,10 +445,10 @@ function showRumorDetailedData(_idx) {
             {
                 title: "分享链接",
                 disabled: isNotUrl, // Optional
-                handler: function() {
+                handler: function () {
                     $share.sheet({
                         items: _url,
-                        handler: function(success) {
+                        handler: function (success) {
                             if (success) {
                                 $ui.toast("分享成功");
                             } else {
@@ -457,7 +460,7 @@ function showRumorDetailedData(_idx) {
             },
             {
                 title: "关闭",
-                handler: function() {}
+                handler: function () {}
             }
         ]
     });
@@ -477,44 +480,52 @@ function getAreaStat(_element) {
         }
     });
 }
+
 function processAreaStatData(_html) {
+    // 省级
     var _list = [];
     _areaStatData = JSON.parse(_html);
     for (x in _areaStatData) {
-        _list.push(_areaStatData[x].provinceName);
+        const thisPro = _areaStatData[x];
+        _list.push(thisPro.provinceName + " (" + thisPro.confirmedCount + "人)");
     }
     return _list;
 }
+
 function showAreaStatData() {
+    // 省级
     $ui.push({
         props: {
             title: "地区排序"
         },
-        views: [
-            {
-                type: "list",
-                props: {
-                    data: _areaStatProData
-                },
-                layout: $layout.fill,
-                events: {
-                    didSelect: function(_sender, indexPath, _data) {
-                        const _idx = indexPath.row;
-                        showAreaStatCityData(_areaStatData[_idx]);
-                    }
+        views: [{
+            type: "list",
+            props: {
+                data: _areaStatProData
+            },
+            layout: $layout.fill,
+            events: {
+                didSelect: function (_sender, indexPath, _data) {
+                    const _idx = indexPath.row;
+                    showAreaStatCityData(_areaStatData[_idx]);
                 }
             }
-        ]
+        }]
     });
 }
+
 function processAreaStatCityData(_json) {
+    // 城市
     var _list = [];
     for (x in _json) {
-        _list.push(_json[x].cityName);
+        const thisCity = _json[x]
+        _list.push(thisCity.cityName + " (" + thisCity.confirmedCount + "人)");
     }
     return _list;
 }
+
 function showAreaStatCityData(_thisPro) {
+    // 城市
     $console.log(_thisPro);
     const _cityList = _thisPro.cities;
     const _cityTitleList = processAreaStatCityData(_cityList);
@@ -522,35 +533,35 @@ function showAreaStatCityData(_thisPro) {
         props: {
             title: _thisPro.provinceName
         },
-        views: [
-            {
-                type: "list",
-                props: {
-                    data: _cityTitleList
-                },
-                layout: $layout.fill,
-                events: {
-                    didSelect: function(_sender, indexPath, _data) {
-                        const _idx = indexPath.row;
-                        const thisItem = _cityList[_idx];
-                        $console.log(thisItem);
-                        $ui.alert({
-                            title: thisItem.cityName,
-                            message:
-                                "确诊人数：" +
-                                thisItem.confirmedCount +
-                                "\n疑似人数：" +
-                                thisItem.suspectedCount +
-                                "\n治愈人数：" +
-                                thisItem.curedCount +
-                                "\n死亡人数：" +
-                                thisItem.deadCount
-                        });
-                    }
+        views: [{
+            type: "list",
+            props: {
+                data: _cityTitleList
+            },
+            layout: $layout.fill,
+            events: {
+                didSelect: function (_sender, indexPath, _data) {
+                    const _idx = indexPath.row;
+                    const thisItem = _cityList[_idx];
+                    $console.log(thisItem);
+                    $ui.alert({
+                        title: thisItem.cityName,
+                        message: "确诊人数：" +
+                            thisItem.confirmedCount +
+                            "\n疑似人数：" +
+                            thisItem.suspectedCount +
+                            "\n治愈人数：" +
+                            thisItem.curedCount +
+                            "\n死亡人数：" +
+                            thisItem.deadCount
+                    });
                 }
             }
-        ]
+        }]
     });
+    if (_thisPro.comment !== "") {
+        $ui.toast(_thisPro.comment);
+    }
 }
 // 开始运行
 initMainMenu();
