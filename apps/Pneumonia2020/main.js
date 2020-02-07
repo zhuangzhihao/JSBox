508462f07696bc0ce061ade200fe4c7ff699d4e3
zhuangzhihao <zhihaofans@hotmail.com>
2020-02-07 18:08:22 +0800

    主页改为代码渲染界面，移除ui模板

--- apps/Pneumonia2020/main.js
+++ apps/Pneumonia2020/main.js
@@ -1,2 +1,55 @@
-$ui.render("main");
+const update = require("./scripts/update");
+const dxy = require("./scripts/dxy");
+const toutiao = require("./scripts/toutiao");
+
+const menuList = ["丁香园", "头条热点"];
+
+function getNavButton() {
+    return [{
+        title: $l10n("CHECK_UPDATE"),
+        icon: "162", // Or you can use icon name
+        symbol: "checkmark.seal", // SF symbols are supported
+        handler: () => {
+            checkUpdate();
+        }
+    }];
+}
+
+function checkUpdate() {
+    const time = Math.round(new Date() / 1000);
+    const serverJsonUrl = "https://cdn.jsdelivr.net/gh/zhuangzhihao/jsbox@master/app.json?t=" + time;
+    const appId = "io.zhihao.jsbox.pneumonia2020";
+    update.checkUpdate(serverJsonUrl, appId);
+}
+
 $console.log("main:init");
+$ui.render({
+    props: {
+        id: "main",
+        homeIndicatorHidden: false,
+        modalPresentationStyle: 0,
+        navButtons: getNavButton()
+    },
+    views: [{
+        type: "list",
+        props: {
+            data: menuList
+        },
+        layout: $layout.fill,
+        events: {
+            didSelect: function (_sender, indexPath, _data) {
+                const _idx = indexPath.row;
+                switch (_idx) {
+                    case 0:
+                        dxy.init();
+                        break;
+                    case 1:
+                        $ui.error("未完成");
+                        break;
+                    default:
+                        $ui.error("错误选项");
+                }
+            }
+        }
+    }]
+});
