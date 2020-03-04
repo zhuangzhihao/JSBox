52208618326ed25f818a264ba5c8367592f84e69
zhihaofans <zhihaofans@hotmail.com>
2020-03-04 22:37:15 +0800

    优化代码

--- apps/jsTool/scripts/view/instagram.js
+++ apps/jsTool/scripts/view/instagram.js
@@ -189,30 +189,47 @@ let instagramOfficial = link => {
         var insUrl = link.split("?")[0];
         $http.get({
             url: `${insUrl}?__a=1`,
+            headers: {
+                "user-agent:": "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.125 Safari/537.36"
+            },
             handler: function (resp) {
                 var data = resp.data;
                 if (data) {
-                    const mediaList = data.graphql.shortcode_media.edge_sidecar_to_children.edges;
-                    if (mediaList) {
-                        if (mediaList.length > 0) {
-                            const insMediaList = mediaList.map(media => {
-                                const thisData = media.node;
-                                return thisData.is_video ?
-                                    new MediaItem("video", thisData.video_url) :
-                                    new MediaItem("image", thisData.display_url);
-                            });
-                            showResultListView(insMediaList);
+                    const _c = data.graphql.shortcode_media.edge_sidecar_to_children;
+                    if (_c) {
+                        const mediaList = _c.edges;
+                        if (mediaList) {
+                            if (mediaList.length > 0) {
+                                const insMediaList = mediaList.map(media => {
+                                    const thisData = media.node;
+                                    return thisData.is_video ?
+                                        new MediaItem("video", thisData.video_url) :
+                                        new MediaItem("image", thisData.display_url);
+                                });
+                                showResultListView(insMediaList);
+                            } else {
+                                $ui.alert({
+                                    title: "解析数据失败",
+                                    message: "媒体列表空白",
+                                });
+                            }
                         } else {
                             $ui.alert({
                                 title: "解析数据失败",
-                                message: "媒体列表空白",
+                                message: "数据空白",
                             });
                         }
                     } else {
+                        const gsm = data.graphql.shortcode_media;
                         $ui.alert({
                             title: "解析数据失败",
-                            message: "数据空白",
+                            message: "数据格式错误，未找到媒体列表，所以只显示封面",
                         });
+                        const insMediaList = [
+                            new MediaItem(gsm.is_video ? "video" : "image",
+                                gsm.display_url)
+                        ];
+                        showResultListView(insMediaList);
                     }
                 } else {
                     $ui.alert({
