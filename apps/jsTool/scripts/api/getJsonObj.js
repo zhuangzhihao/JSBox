b77d965a4e4800563dfd98d488a85b9012deb2b3
zhihaofans <zhihaofans@hotmail.com>
2020-03-08 21:47:57 +0800

    更新代码

--- apps/jsTool/scripts/api/getJsonObj.js
+++ apps/jsTool/scripts/api/getJsonObj.js
@@ -1,14 +1,23 @@
-const jsonData = {
-    test:"test",
-    
+// 可以在node.js环境运行
+const jsonData = {};
+
+let gen_1 = (funId) => {
+    const keys = Object.keys(jsonData);
+    var str = `function ${funId}(${keys.toString()}){`;
+    keys.map(i => {
+        str += `\nthis.${i}=${i};`;
+    });
+    str += "};"
+    console.info(str);
 };
-const funId = "";
 
-
-const keys = Object.keys(jsonData);
-var str = `function ${funId}(${keys.toString()}){`;
-keys.map(i => {
-    str += `\nthis.${i}=${i};`;
-});
-str += "};"
-console.info(str);
+let gen_2 = (funId, objId) => {
+    const keys = Object.keys(jsonData);
+    var str = `function ${funId}(${objId}){`;
+    keys.map(i => {
+        str += `\nthis.${i} = ${objId}.${i};`;
+    });
+    str += "};"
+    console.info(str);
+};
+// gen_2("BasicData", "_basicData");
