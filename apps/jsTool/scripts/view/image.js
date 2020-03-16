5c299d6e8583b67a85a92fb06ebf9a17f0407641
zhihaofans <zhihaofans@hotmail.com>
2020-03-16 21:56:26 +0800

    更新必应每日图片

--- apps/jsTool/scripts/view/image.js
+++ apps/jsTool/scripts/view/image.js
@@ -1,5 +1,22 @@
 let app = require("../api/app.js");
-
+let todayImageIoliu = () => {
+    const screenInfo = $device.info.screen;
+    const url = `https://bing.ioliu.cn/v1?w=${screenInfo.width}&h=${screenInfo.height}`;
+    $console.info(url);
+    $ui.preview({
+        title: "bing.ioliu.cn",
+        url: url
+    });
+};
+let randomImageIoliu = () => {
+    const screenInfo = $device.info.screen;
+    const url = `https://bing.ioliu.cn/v1/rand?w=${screenInfo.width}&h=${screenInfo.height}`;
+    $console.info(url);
+    $ui.preview({
+        title: "bing.ioliu.cn",
+        url: url
+    });
+};
 let girlImage = () => {
     const url = "https://api.isoyu.com/mm_images.jsp";
     $ui.preview({
@@ -8,13 +25,13 @@ let girlImage = () => {
     });
 };
 
-let bingDailyImage = () => {
+let bingDailyImageIsoyu = () => {
     const url = "https://api.isoyu.com/bing_images.jsp";
-    /* $ui.preview({
+    $console.info(url);
+    $ui.preview({
         title: "Bing daily image",
         url: url
-    }); */
-    $quicklook.open(url);
+    });
 };
 
 let docScan = () => {
@@ -77,20 +94,32 @@ let init = () => {
         views: [{
             type: "list",
             props: {
-                data: app.getListFromL10n(["SCAN_DOCUMENTS"])
+                data: [{
+                    title: "ioliu必应图片",
+                    rows: app.getListFromL10n(["今天图片", "随机图片"])
+                }, {
+                    title: "其他",
+                    rows: app.getListFromL10n(["SCAN_DOCUMENTS"])
+                }, ]
             },
             layout: $layout.fill,
             events: {
                 didSelect: function (_sender, indexPath, _data) {
-                    switch (indexPath.row) {
+                    const section = indexPath.section;
+                    const row = indexPath.row;
+                    switch (section) {
                         case 0:
-                            docScan();
+                            switch (row) {
+                                case 0:
+                                    todayImageIoliu();
+                                    break;
+                                case 1:
+                                    randomImageIoliu();
+                                    break;
+                            }
                             break;
                         case 1:
-                            bingDailyImage();
-                            break;
-                        case 2:
-                            girlImage();
+                            docScan();
                             break;
                     }
                 }
